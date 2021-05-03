65ceb67d118304d0b44fb5859dd2c295b3d7eea9
MWPX777 <mwpx777@gmail.com>
2021-05-03 08:06:00 -0500

    finished with 21.4

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,47 @@
+// APP
+
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-// apolloProvider provides data to all other components
-import {ApolloProvider} from '@apollo/react-hooks';
-// apolloClient gets the info ready when ready to use it
-import ApolloClient from 'apollo-boost';
-
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
-// establish connection to GraphQL server using Apollo
 const client = new ApolloClient({
   uri: '/graphql'
 });
 
 function App() {
   return (
-    // passing client prop to provider to have access to server's API data
     <ApolloProvider client={client}>
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
-      </div>
-      <Footer />
-    </div>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
 
 export default App;
+
